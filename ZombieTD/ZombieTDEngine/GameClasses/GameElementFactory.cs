5c69eb6905c9e531e213268dc9c6d3a84cdb7eed
jdosdos <jdosd150@uwsp.edu>
2013-10-28 19:36:41 -0500

    Create GameElementFactory.cs

---
+++ ZombieTD/ZombieTDEngine/GameClasses/GameElementFactory.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZombieTDEngine.GameInterfaces;
+
+namespace ZombieTDEngine.GameClasses
+{
+    class GameElementFactory
+    {
+        public IGameElement MakeZombie()
+        {
+            return null;
+        }
+
+        public IGameElement MakeZombieDog()
+        {
+            return null;
+        }
+
+        public IGameElement MakeFlyingZombie()
+        {
+            return null;
+        }
+
+        public IGameElement MakeRedneck()
+        {
+            return null;
+        }
+
+        public IGameElement MakeSheriff()
+        {
+            return null;
+        }
+
+        public IGameElement MakePriest()
+        {
+            return null;
+        }
+
+        public IGameElement MakeHay()
+        {
+            return null;
+        }
+
+        public IGameElement MakeCar()
+        {
+            return null;
+        }
+
+        public IGameElement MakePit()
+        {
+            return null;
+        }
+    }
+}
